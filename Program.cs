95c91abb73a4d46b95a34b86e5b7082ddc452ff5
Jonesey86 <irj082024@gmail.com>
2025-01-29 15:57:05 +0100

    changes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Welcome to the Whack a Mole game!");
     }
 }
